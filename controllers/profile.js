230f329638fd228ed6bb1726efbe55ff79a53119
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-09-12 19:58:40 -0400

    added handle column to users db and modified profileupdatewithphoto

--- controllers/profile.js
+++ controllers/profile.js
@@ -26,7 +26,40 @@ const handleProfileUpdate = (req, res, db) => {
     })
     .catch(err => res.status(400).json('Error updating user'))
 }
+
+const handleProfileUpdateWithPhoto = (req, res, db) => {
+  const { id } = req.params;
+  const { name, age, pet, photourl } = req.body.formInput;
+
+  if (photourl === '' || photourl === undefined) {
+    console.log(photourl)
+    return db('users')
+    .where({ id })
+    .update({ name, age, pet })
+    .then(resp => {
+      if(resp) {
+        res.json("success")
+      } else {
+        res.status(400).json('Unable to update')
+      }
+    })
+    .catch(err => res.status(400).json('Error updating user'))
+  }
+  return db('users')
+    .where({ id })
+    .update({ name, age, pet, photourl })
+    .then(resp => {
+      if(resp) {
+        res.json("success")
+      } else {
+        res.status(400).json('Unable to update')
+      }
+    })
+    .catch(err => res.status(400).json('Error updating user'))
+}
+
 module.exports = {
   handleProfileGet,
-  handleProfileUpdate
+  handleProfileUpdate,
+  handleProfileUpdateWithPhoto
 }
